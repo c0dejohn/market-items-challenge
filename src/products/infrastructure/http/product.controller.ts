41ae18532923e0c3c3081bbca972751c2d2935ab
Edwin Manyoma <jemanyoma07@gmail.com>
2026-01-12 02:24:54 -0500

    feat: implement in-memory caching for product details

--- src/products/infrastructure/http/product.controller.ts
+++ src/products/infrastructure/http/product.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 import { GetProductDetailHandler } from '../../application/get-product-detail.handler';
 import { Product } from '../../domain/product.model';
 
 @ApiTags('items')
 @Controller('items')
+@UseInterceptors(CacheInterceptor)
 export class ProductController {
     constructor(private readonly getProductDetailHandler: GetProductDetailHandler) { }
 
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { ProductController } from './infrastructure/http/product.controller';
 import { GetProductDetailHandler } from './application/get-product-detail.handler';
 import { FileSystemProductRepository } from './infrastructure/persistence/file-system-product.repository';
 import { PRODUCT_REPOSITORY } from './domain/product.repository.interface';
 
 @Module({
+    imports: [CacheModule.register()],
     controllers: [ProductController],
     providers: [
         GetProductDetailHandler,
